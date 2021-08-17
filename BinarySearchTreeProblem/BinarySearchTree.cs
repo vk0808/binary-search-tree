6bbd74e093bbb5d078bf3a901f1f024f463b183a
vk0808 <vk4765712@gmail.com>
2021-08-17 18:36:11 +0530

    feat: created BST

---
+++ BinarySearchTreeProblem/BinarySearchTree.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTreeProblem
+{
+    public class BinarySearchTree<T> where T : IComparable<T>
+    {
+        // instance variables
+        public T NodeData { get; set; }
+        public BinarySearchTree<T> LeftTree { get; set; }
+        public BinarySearchTree<T> RightTree { get; set; }
+        private int leftCount = 0, rightCount = 0;
+
+
+        // constructor
+        public BinarySearchTree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.RightTree = null;
+            this.LeftTree = null;
+        }
+
+        // method to insert
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.LeftTree == null)
+                    this.LeftTree = new BinarySearchTree<T>(item);
+                else
+                    this.LeftTree.Insert(item);
+            }
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new BinarySearchTree<T>(item);
+                else
+                    this.RightTree.Insert(item);
+            }
+        }
+        
+        
+        // method to display
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();
+            }
+        }
+    }
+}
--- BinarySearchTreeProblem/Program.cs
+++ BinarySearchTreeProblem/Program.cs
@@ -6,7 +6,12 @@ namespace BinarySearchTreeProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Binary Search Program");
+            Console.WriteLine("Binary Search Program\n");
+
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
         }
     }
 }
