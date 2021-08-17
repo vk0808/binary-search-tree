c3b5199baa41bbb3a1f16b4f61db8c6e86499bc8
vk0808 <vk4765712@gmail.com>
2021-08-17 18:49:58 +0530

    feat: recreate bst

--- BinarySearchTreeProblem/Program.cs
+++ BinarySearchTreeProblem/Program.cs
@@ -9,8 +9,18 @@ namespace BinarySearchTreeProblem
             Console.WriteLine("Binary Search Program\n");
 
             BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(3);
+            binarySearch.Insert(11);
+            binarySearch.Insert(16);
+            binarySearch.Insert(22);
             binarySearch.Insert(30);
+            binarySearch.Insert(40);
+            binarySearch.Insert(60);
+            binarySearch.Insert(63);
+            binarySearch.Insert(65);
+            binarySearch.Insert(67);
             binarySearch.Insert(70);
+            binarySearch.Insert(95);
             binarySearch.Display();
         }
     }
